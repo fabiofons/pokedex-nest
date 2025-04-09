72de4ed1c47754310c52764990fdf098242e1701
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-04-09 14:00:22 -0500

    Dependency Injection

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,18 +4,27 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
+
   async fillDB(): Promise<string> {
     await this.pokemonModel.deleteMany();
 
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=650');
-    const data = (await response.json()) as PokeResponse;
+    // axios way
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    //fetch way
+    // const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=650');
+    // const data = (await response.json()) as PokeResponse;
 
     // const insertPokePromises: Promise<CreatePokemonDto>[] = []; //slowy way
     const pokemonToInsert: { name: string; number: number }[] = [];
