5064350803c6bef22710d7fc5f28eabadbe4b73e
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-04-11 13:55:08 -0500

    added validation schema using Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
-import { join } from 'path';
+import { JoiValidationSchema } from './config/joi.validationSchema';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { appConfig } from './config/app.config';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig],
+      validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
