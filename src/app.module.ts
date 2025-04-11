4703e29a1d775804f350a3ad500ff2f00c7de54c
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2025-04-10 22:59:47 -0500

    configure configModule for env var access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,22 @@
-import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokedex'),
+    MongooseModule.forRoot(
+      process.env.MONGODB || 'mongodb://localhost:27017/nest-pokemon',
+    ),
     PokemonModule,
     CommonModule,
     SeedModule,
